a3fea3068c236ad5235ad0da608388d28eb0dc2e
Ankit Pandey <pandeyan@grinnell.edu>
2017-02-04 14:58:14 -0600

    Added WordGenerator

---
+++ src/WordGenerator.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+
+public class WordGenerator {
+	private Scanner openFile;
+	private int wordCounter, sentenceCounter;
+	
+	public WordGenerator(String filename) throws IOException {
+		openFile = new Scanner(new File(filename));
+	}
+	
+	public boolean hasNext() {
+		return openFile.hasNext();
+	}
+	
+	public String next() {
+		String nextWord = openFile.next();
+		if (nextWord.contains(".") || nextWord.contains("?") || nextWord.contains("!"))
+			sentenceCounter++;
+		wordCounter++;
+		System.out.println(wordCounter);
+		return nextWord;
+	}
+	
+	int getWordCount() {
+		return wordCounter;
+	}
+	
+	int getSentenceCount() {
+		return sentenceCounter;
+	}
+}
