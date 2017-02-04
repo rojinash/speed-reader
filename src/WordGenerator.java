c0bb2b95aff40ebb7f9b6700fb9aee7e4158a027
Ankit Pandey <pandeyan@grinnell.edu>
2017-02-04 15:30:25 -0600

    Added comments to WordGenerator class

--- src/WordGenerator.java
+++ src/WordGenerator.java
@@ -13,19 +13,28 @@ public class WordGenerator {
 	public boolean hasNext() {
 		return openFile.hasNext();
 	}
-	
+	/* 
+	 * @return Returns the next word of the WordGenerator.
+	 */
 	public String next() {
 		String nextWord = openFile.next();
 		if (nextWord.contains(".") || nextWord.contains("?") || nextWord.contains("!"))
 			sentenceCounter++;
 		wordCounter++;
+		System.out.println(wordCounter);
 		return nextWord;
 	}
 	
+	/*
+	 * @return Returns the number of words produced by the current WordGenerator so far.
+	 */
 	int getWordCount() {
 		return wordCounter;
 	}
 	
+	/*
+	 * Returns the total number of sentences produced by the current WordGenerator so far.
+	 */
 	int getSentenceCount() {
 		return sentenceCounter;
 	}
